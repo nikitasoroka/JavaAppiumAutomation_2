8f8eb97b327600dd049149128caaad2a7008be67
nikitasoroka <nikitasoroka94@gmail.com>
2019-12-11 19:32:50 +0300

    Ex1: Configuration of the instruments

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -22,7 +22,7 @@ public class FirstTest {
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","C:\\Users\\Mikita.Saroka\\Desktop\\JavaAppiumAutomation\\apks\\wikipedia.apk");
+        capabilities.setCapability("app","C:\\Users\\Mikita.Saroka\\Desktop\\JavaAppiumAutomation_2\\apks\\wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
