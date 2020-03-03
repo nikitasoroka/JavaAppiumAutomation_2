d6ff5d98b6dcfc59964d120cd54b88b9d397d4ff
nikitasoroka <nikitasoroka94@gmail.com>
2020-03-03 12:48:11 +0300

    2 lesson!!!

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,12 @@
 import  io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -37,8 +40,43 @@ public class FirstTest{
         driver.quit();
     }
 
+//    @Test
+//    public void firstTest()
+//    {
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Skip')]"),
+//                "Cannot find Skip button",
+//                5
+//        );
+//
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find search input",
+//                10
+//        );
+//
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find search input",
+//                10
+//        );
+//
+//        waitForElementAndSendKeys(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Java",
+//                "Cannot input the text",
+//                20
+//        );
+//
+//        waitForElementPresent(
+//                By.xpath("//*[@class='android.view.ViewGroup']//*[@text='Object-oriented programming language']"),
+//                "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
+//                15
+//        );
+//    }
+
     @Test
-    public void firstTest()
+    public void swipeArticle()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Skip')]"),
@@ -52,27 +90,89 @@ public class FirstTest{
                 5
         );
 
+        waitForElementPresent(
+                By.xpath("//*[@class='android.widget.EditText']//*[@text='Search Wikipedia']"),
+                "Cannot find the search field",
+                10);
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find search input",
+                10
+        );
+
         waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Java",
                 "Cannot input the text",
-                5
+                20
         );
-
-//        waitForElementAndClear(
-//                By.id("org.wikipedia:id/search_src_text"),
-//                "Cannot find search field",
-//                5
-//
-//        );
 
         waitForElementPresent(
                 By.xpath("//*[@class='android.view.ViewGroup']//*[@text='Object-oriented programming language']"),
                 "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
                 15
         );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.view.ViewGroup']//*[@text='Object-oriented programming language']"),
+                "Cannot open the 'Object-oriented programming language' topic searching by 'Java' link",
+                15
+        );
+
+        waitForElementPresent(
+                By.xpath("//android.view.View[@content-desc=\"Java (programming language)\"]"),
+                "Cannot find the opened page",
+                15
+                );
+
+//        swipeUp(2000);
+//        swipeUp(2000);
+//        swipeUp(2000);
+//        swipeUp(2000);
+//        swipeUp(2000);
+
     }
 
+//    @Test
+//    public void searchAndClear() {
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Skip')]"),
+//                "Cannot find Skip button",
+//                5
+//        );
+//
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find search input",
+//                5
+//        );
+//
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find search input",
+//                15
+//        );
+//
+//        waitForElementAndSendKeys(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Java",
+//                "Cannot input the text",
+//                20
+//        );
+//
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Java')]"),
+//                "Cannot find search input",
+//                15
+//        );
+//
+//        waitForElementAndClear(
+//                By.xpath("//*[contains(@text,'Java')]"),
+//                "Cannot find search input",
+//                10
+//        );
+//    }
 //    @Test
 //
 //    public void testCompareArticleTitle()
@@ -90,59 +190,62 @@ public class FirstTest{
 //        );
 //
 //        waitForElementAndSendKeys(
-//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
 //                "Java",
 //                "Cannot input the text",
+//                30
+//        );
+//
+//        waitForElementAndClick(
+//                By.xpath("//*[@class='android.view.ViewGroup']//*[@text='Object-oriented programming language']"),
+//                "Cannot open the 'Java' link",
+//                30
+//        );
+//
+//        waitForElementPresent(
+//                By.xpath(),
+//                "Cannot find article title",
+//                15
+//
+//        );
+//    }
+
+//    @Test
+//
+//    public void testCancelSearch()
+//    {
+//        waitForElementAndClick(
+//                By.id("//*[contains(@text,'Skip')]"),
+//                "Cannot find Skip button",
 //                5
 //        );
 //
 //        waitForElementAndClick(
-//                By.xpath(""),
-//                "Cannot open the 'Java' link",
-//                15
+//                By.id("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find search input",
+//                5
 //        );
 //
-//        waitForElementPresent(
+//        waitForElementAndSendKeys(
+//                By.id("org.wikipedia:id/search_src_text"),
+//                "Appium",
+//                "Cannot input the text",
+//                10
+//        );
 //
-//        )
+//        waitForElementAndClick(
+//                By.id("org.wikipedia:id/search_close_btn"),
+//                "Close button doesn't work",
+//                5
+//        );
+//
+//        waitForElementNotPresent(
+//                By.id("rg.wikipedia:id/search_close_btn"),
+//                "X is still presented on the page",
+//                5
+//        );
 //    }
 
-  /*  @Test
-
-    public void testCancelSearch()
-    {
-        waitForElementAndClick(
-                By.id("//*[contains(@text,'Skip')]"),
-                "Cannot find Skip button",
-                5
-        );
-
-        waitForElementAndClick(
-                By.id("//*[contains(@text,'Search Wikipedia')]"),
-                "Cannot find search input",
-                5
-        );
-
-        waitForElementAndSendKeys(
-                By.id("org.wikipedia:id/search_src_text"),
-                "Appium",
-                "Cannot input the text",
-                10
-        );
-
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/search_close_btn"),
-                "Close button doesn't work",
-                5
-        );
-
-        waitForElementNotPresent(
-                By.id("rg.wikipedia:id/search_close_btn"),
-                "X is still presented on the page",
-                5
-        );
-    }
-*/
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -165,21 +268,33 @@ public class FirstTest{
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.sendKeys(value);
         return element;
-    }
+    }}
 
-    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds)   // метод который показывает, что мы ожидаем, что элемента "X" больше не будет на странице
-    {
-     WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-     wait.withMessage(error_message + "\n");
-     return wait.until(
-             ExpectedConditions.invisibilityOfElementLocated(by)
-     );
-    }
+//    protected void swipeUp (int timeOfSwipe)
+//    {
+//        TouchAction action = new TouchAction(driver);
+//        Dimension size = driver.manage().window().getSize();
+//        int x = size.width / 2;
+//        int start_y = (int) (size.height * 0.8);
+//        int end_y = (int) (size.height * 0.2);
+//
+//        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).perform();
+//    }
+//}
 
-    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
-    {
-        WebElement element = waitForElementAndClear(by, error_message, timeoutInSeconds);
-        element.clear();
-        return element;
-    }
-}
+//    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds)   // метод который показывает, что мы ожидаем, что элемента "X" больше не будет на странице
+//    {
+//     WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+//     wait.withMessage(error_message + "\n");
+//     return wait.until(
+//             ExpectedConditions.invisibilityOfElementLocated(by)
+//     );
+//    }
+
+//    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
+//    {
+//        WebElement element = waitForElementAndClear(by, error_message, timeoutInSeconds);
+//        element.clear();
+//        return element;
+//    }
+//}
